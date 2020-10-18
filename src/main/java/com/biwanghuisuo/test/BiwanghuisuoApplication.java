7ea1392446a663ce927d89b5e07767d84987222f
2333shang <1152761842@qq.com>
2020-10-18 18:51:40 +0800

    first

---
+++ src/main/java/com/biwanghuisuo/test/BiwanghuisuoApplication.java
@@ -0,0 +1,13 @@
+package com.biwanghuisuo.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiwanghuisuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiwanghuisuoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biwanghuisuo/test/BiwanghuisuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biwanghuisuo.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiwanghuisuoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

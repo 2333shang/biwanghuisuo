bd7d12359ef2989b548d03dc47b568d299b731fa
2333shang <1152761842@qq.com>
2020-10-19 20:39:38 +0800

    third

--- src/main/java/com/biwanghuisuo/test/controller/HelloController.java
+++ src/main/java/com/biwanghuisuo/test/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.biwanghuisuo.test.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +13,26 @@ public class HelloController {
 	public String test() {
 		System.out.println(111);
 		return "Hello World! 2333";
+	}
+
+	@RequestMapping("/bushu")
+	public String bushu() {
+		String result = null;
+		try {
+			String shpath = "/biwanghuisuo/bushu.sh";
+			Process ps = Runtime.getRuntime().exec(shpath);
+			ps.waitFor();
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(ps.getInputStream()));
+			StringBuffer sb = new StringBuffer();
+			String line;
+			while ((line = br.readLine()) != null) {
+				sb.append(line).append("\n");
+			}
+			result = sb.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 }

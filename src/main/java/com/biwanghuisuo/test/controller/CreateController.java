66082618103abfd68c6b1616c57f625aa07a506a
2333shang <1152761842@qq.com>
2020-10-24 19:36:19 +0800

    add download

--- src/main/java/com/biwanghuisuo/test/controller/CreateController.java
+++ src/main/java/com/biwanghuisuo/test/controller/CreateController.java
@@ -1,19 +1,22 @@
 package com.biwanghuisuo.test.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.StringWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.StringWriter;
 
 @Controller
 public class CreateController {
@@ -44,7 +47,7 @@ public class CreateController {
 
         StringWriter sw = new StringWriter();
 
-        t.merge(ctx, sw);
+        t.merge(ctx, sw); 
 
         String path = getClass().getResource("/").getPath();
         File file = new File(path);
@@ -54,6 +57,32 @@ public class CreateController {
         System.out.println(path + controller + "Controller.java");
         fos.write(sw.toString().getBytes());
         response.setContentType("text/html;charset=utf-8");
-        response.getOutputStream().print(sw.toString());
+        response.getWriter().write(sw.toString());
+    }
+    
+    @RequestMapping("/createFile")
+    public ResponseEntity<byte[]> createFile(String controller, String method, String say) throws Exception {
+    	VelocityEngine ve = new VelocityEngine();
+    	ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+    	ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+    	
+    	ve.init();
+    	
+    	Template t = ve.getTemplate("BaseController.vm");
+    	VelocityContext ctx = new VelocityContext();
+    	
+    	ctx.put("className", controller);
+    	ctx.put("request", method);
+    	ctx.put("say", say);
+    	
+    	StringWriter sw = new StringWriter();
+    	
+    	t.merge(ctx, sw); 
+    	
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attchement;filename=" + controller + "Controller.java");
+        HttpStatus statusCode = HttpStatus.OK;
+        ResponseEntity<byte[]> entity = new ResponseEntity<>(sw.toString().getBytes(), headers, statusCode);
+        return entity;
     }
 }
